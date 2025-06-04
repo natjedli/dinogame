f2ac05e899e9ab8b33a6dd5a9365a0e07026a1d7
natjedli <naty.ann.jedlicka@seznam.cz>
2025-06-04 10:50:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function jump() {
   if (isJumping) return;
   isJumping = true;
 
-  let jumpPeak = dinoBottom + 120;
+  let jumpPeak = dinoBottom + 80;
   let upInterval = setInterval(() => {
     if (dinoBottom >= jumpPeak) {
       clearInterval(upInterval);
@@ -46,14 +46,13 @@ function createObstacle() {
   const obstacle = document.createElement("div");
   obstacle.classList.add("obstacle");
 
-  let height = 30 + Math.random() * 30;
-  let width = 20 + Math.random() * 20;
+  let height = 30 + Math.random() * 50;
+  let width = 20 + Math.random() * 30;
   obstacle.style.height = height + "px";
   obstacle.style.width = width + "px";
 
   let obstacleLeft = window.innerWidth;
   obstacle.style.left = obstacleLeft + "px";
-  obstacle.style.bottom= 0px
   gameContainer.appendChild(obstacle);
 
   let moveInterval = setInterval(() => {
@@ -65,21 +64,16 @@ function createObstacle() {
     obstacleLeft -= speed;
     obstacle.style.left = obstacleLeft + "px";
 
-const dinoRect = dino.getBoundingClientRect();
-const obstacleRect = obstacle.getBoundingClientRect();
-
-const isColliding =
-  dinoRect.right > obstacleRect.left &&
-  dinoRect.left < obstacleRect.right &&
-  dinoRect.bottom > obstacleRect.top &&
-  dinoRect.top < obstacleRect.bottom;
-
-if (isColliding) {
-  clearInterval(moveInterval);
-  gameOver = true;
-  alert("💥 GAME OVER! Skóre: " + score);
-  location.reload();
-}
+    if (
+      obstacleLeft < 100 &&
+      obstacleLeft + width > 50 &&
+      dinoBottom < height
+    ) {
+      clearInterval(moveInterval);
+      gameOver = true;
+      alert("💥 GAME OVER! Skóre: " + score);
+      location.reload();
+    }
 
     if (obstacleLeft < -width) {
       clearInterval(moveInterval);
@@ -93,4 +87,4 @@ if (isColliding) {
 
 setInterval(() => {
   if (!gameOver) createObstacle();
-}, 1500); 
+}, 1500);
