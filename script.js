4007a1477fe0c7eff80ecebf0cb4a00203ff734b
natjedli <naty.ann.jedlicka@seznam.cz>
2025-06-04 11:02:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ function createObstacle() {
   const obstacle = document.createElement("div");
   obstacle.classList.add("obstacle");
 
-  let height = 30 + Math.random() * 50;
-  let width = 20 + Math.random() * 30;
+  let height = 20 + Math.random() * 15;
+  let width = 10 + Math.random() * 5;
   obstacle.style.height = height + "px";
   obstacle.style.width = width + "px";
 
